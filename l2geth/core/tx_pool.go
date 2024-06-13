ec589044a5978ffed08c34c4464d004e91b971f5
Aiden <aiden.a@metis.io>
2024-06-14 00:20:21 +0800

    l2geth: mempool underprice

--- l2geth/core/tx_pool.go
+++ l2geth/core/tx_pool.go
@@ -550,10 +550,10 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrInvalidSender
 	}
 	// Drop non-local transactions under our own minimal accepted gas price
-	local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
-	if !local && pool.gasPrice.Cmp(tx.GasPrice()) > 0 {
-		return ErrUnderpriced
-	}
+	// local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
+	// if !local && pool.gasPrice.Cmp(tx.GasPrice()) > 0 {
+	// 	return ErrUnderpriced
+	// }
 	// Ensure the transaction adheres to nonce ordering
 	if rcfg.UsingOVM {
 		if rcfg.DeSeqBlock > 0 && pool.chain.CurrentBlock().NumberU64()+1 >= rcfg.DeSeqBlock {
